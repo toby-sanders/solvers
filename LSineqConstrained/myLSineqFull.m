b01abca28f758a54c140bba218adc1c61a2327e7
Toby Sanders <sandertl20@gmail.com>
2022-06-06 12:01:39 -0500

    gradient based full solver complete: LM step size still unknown

--- LSineqConstrained/myLSineqFull.m
+++ LSineqConstrained/myLSineqFull.m
@@ -1,4 +1,4 @@
-function [x,out] = myLSineq(A,b,C,d,opts)
+function [x,out] = myLSineq(A,b,E,e,C,d,opts)
 
 % currently solving the following minimization
 % min 0.5 ||Ax-b||^2 s.t. Cx - d >= 0
@@ -7,30 +7,34 @@ function [x,out] = myLSineq(A,b,C,d,opts)
 
 if ~isfield(opts,'iter'), opts.iter = 100; end
 if ~isfield(opts,'tol'), opts.tol = 1e-5; end
-if ~isfield(opts,'LMupdate'), opts.LMupdate = 10; end
+if ~isfield(opts,'LMupdate'), opts.LMupdate = 2; end
 if ~isfield(opts,'gam'), opts.gam = 1e-2; end
 
 if ~isa(A,'function_handle'), A = @(u,mode) f_handleA(A,u,mode); end
+if ~isa(E,'function_handle'), E = @(u,mode) f_handleA(E,u,mode); end
 if ~isa(C,'function_handle'), C = @(u,mode) f_handleA(C,u,mode); end
 
 gam = opts.gam; % step size for LM update
 
 Atb = A(b,2);
 x = zeros(size(Atb));
-lambda = zeros(size(d));
+lambda = zeros(numel(e),1);
+mu = zeros(numel(d),1);
 out.rel_chg = [];
 g2 = 0;
+g3 = 0;
 % AtA = A'*A;
 for ii = 1:opts.iter
     % gradient over quadratic term   
     g1 = A(A(x,1),2) - Atb;
-    g = g1+g2;
+    g = g1+g2+g3;
     
     % get step length, tau
     if ii==1 
         Ag = A(g,1);
+        Eg = E(g,1);
         Cg = C(g,1);
-        tau = (g'*g1 + lambda'*Cg)/(Ag'*Ag); % step length
+        tau = (g'*g1 + lambda'*Eg + mu'*Cg)/(Ag'*Ag); % step length
         if isnan(tau)
             tau = 1e-5;
         end
@@ -47,10 +51,12 @@ for ii = 1:opts.iter
     % Lagrange multiplier update 
     % multipliers for positive inequality contraint are non-positive
     if mod(ii,opts.LMupdate)==0 
-        lambda = lambda + gam*(C(x,1)-d);% gam*(C*x-d); 
-        lambda = min(lambda,0); 
+        lambda = lambda + gam*(E(x,1)-e);% gam*(C*x-d); 
+        mu = mu + gam*(C(x,1)-d);
+        mu = min(mu,0);
 
-        g2 = C(lambda,2);% C'*lambda;   % update gradient on LM term
+        g2 = E(lambda,2);% C'*lambda;   % update gradient on LM term
+        g3 = C(mu,2);
     end
     out.rel_chg = [out.rel_chg;myrel(x,xp)]; 
 
--- LSineqConstrained/testmyLSineqNew.m
+++ LSineqConstrained/testmyLSineqNew.m
@@ -1,10 +1,11 @@
 % testing to minimize ||Ax-b||^2 subject to Cx-d>=0
+clear;
 d = 200; % signal dimension, x
 m = 500; % number of least squares terms/rows in A
 c = 50; % number of inequality constraints
-e = 0; % number of equality contraints
+e = 50; % number of equality contraints
 rng(321);
-opts.iter = 10000;
+opts.iter = 20000;
 opts.tol = 1e-8;
 
 % randomly generate A, b, C, and D
@@ -15,10 +16,16 @@ dv = rand(c,1);
 E = rand(e,d);
 ev = rand(e,1);
 
-
-[x,out] = myLSineq2(A,b,E,ev,C,dv,opts);
+opts.gam = 1e-2;
+opts.LMupdate = 2;
+[x,out] = myLSineqFull(A,b,E,ev,C,dv,opts);
 [x2,out2] = myLSineq(A,b,C,dv,opts);
 
+eq1 = E*x-ev;
+eq2 = E*x2-ev;
+
+ineq1 = C*x-dv;
+ineq2 = C*x2-dv;
 %%
 ineqE = C*x-dv;
 ineqE2 = C*x2-dv;
@@ -26,8 +33,20 @@ figure(23);tiledlayout(2,2);
 nexttile;hold off;
 plot(ineqE,'o');hold on;
 plot(ineqE2,'x');
-plot(out.activeIneqs,'*');hold off;
-nexttile;
-plot(out.x0);
-nexttile;
-semilogy(out.GD.rel_chg);
+legend('full solver','inequality only solver')
+title('recovered values');
+hold off;
+nexttile;hold off;
+semilogy(out.rel_chg);hold on;
+semilogy(out2.rel_chg);hold off;
+legend('full solver','inequality only solver')
+nexttile;hold off;
+semilogy(abs(eq1));hold on;
+semilogy(abs(eq2),':r');hold off;
+title('equality contraint values');
+legend('full solver','inequality only solver')
+nexttile;hold off;
+plot((ineq1));hold on;
+plot((ineq2),':r');
+title('inequality contraint values');
+legend('full solver','inequality only solver')
